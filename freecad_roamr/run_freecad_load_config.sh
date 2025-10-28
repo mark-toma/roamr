98b5440c76322f1a14f45d591a12dffcefa18476
Mark Tomaszewski <mark@mark-toma.com>
2025-10-28 17:38:55 -0600

    Add POC script for loading YAML in FreeCAD macro

---
+++ freecad_roamr/run_freecad_load_config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+FREECAD_CMD="FreeCAD_1.0.0RC2-conda-Linux-x86_64-py311.AppImage"
+
+$FREECAD_CMD freecadcmd "$(dirname $0)/macros/load_config.py" --pass \
+  config_file="$(dirname $0)/roamr_geometry.yaml" \
+  freecad_file="$(dirname $0)/yaml.FCStd"
