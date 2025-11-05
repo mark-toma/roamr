0082b9d81acf7b64e476422a301b72aee6c73920
Mark Tomaszewski <mark@mark-toma.com>
2025-11-04 19:04:57 -0700

    Update sim package with boilerplate content

---
+++ roamr_sim/README.md
@@ -0,0 +1,14 @@
+# ROAMeR Simulation
+
+<!-- TODO: Improve summary when funcitonality is more mature -->
+Robot simulation package for ROAMeR providing Gazebo simulation.
+
+## Usage
+
+<!-- TODO -->
+
+## Development Constraints
+
+This package depends on the base URDF macro(s) in `roamr_description` through their extension to
+Gazebo. This package may also come to depend upon actuation and sensing configurations that match
+the physical robot design.
--- roamr_sim/setup.py
+++ roamr_sim/setup.py
@@ -1,22 +1,45 @@
+# Copyright 2025 Mark Tomaszewski
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import find_packages, setup
 
 package_name = 'roamr_sim'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
+    # TODO: clean up commented lines below
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*launch.[pxy][yma]*'))
+        ),
+        # (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        # (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        # (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='marktoma',
+    maintainer='Mark Tomaszewski',
     maintainer_email='mark@mark-toma.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Gazebo simulation support for ROAMeR',
+    license='Apache 2.0',
     extras_require={
         'test': [
             'pytest',
